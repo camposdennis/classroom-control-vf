2e5990195f427086f571213fd41374da76f8ae16
camposdennis <camposdennis37@gmail.com>
2016-03-30 15:31:27 -0600

    Update managed_user.pp

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -1,12 +1,32 @@
+
+# $REPO/site/users/manifests/managed_user.pp
+
 define users::managed_user (
-  $group = $title,
-){
+  $username  = $title,
+  $uid       = undef,
+  $groupname = $title,
+  $homedir   = "/home/${username}",
+) {
+  File {
+    owner  => $username,
+    group  => $groupname,
+    mode   => '0644',
+  }
+
   user { $title:
     ensure => present,
+    uid    => $uid,
+    gid    => $groupname,
+    home   => $homedir,
+    name   => $username,
   }
-  file { "/home/${title}":
-    ensure => directory,
-    owner => $title,
-    group => $group,
+
+  group { $title:
+    ensure => present,
+    name   => $groupname,
+  }
+
+  file { [ $homedir, "${homedir}/.ssh" ]: 
+    ensure => directory
   }
 }
