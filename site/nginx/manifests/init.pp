eec208c238736e82a50cb0a2d4e494e4bef0b7fd
camposdennis <camposdennis37@gmail.com>
2016-03-30 15:27:51 -0600

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -2,32 +2,38 @@ class nginx {
   File {
     owner => 'root',
     group => 'root',
-    mode => '0664',
+    mode  => '0644',
+    require => Package['nginx'],
+    notify  => Service['nginx'],
   }
+
+  $confdir = '/etc/nginx'
+  
   package { 'nginx':
-    ensure => present,
+    ensure => installed,
   }
-  file { [ '/var/www', '/etc/nginx/conf.d' ]:
-    ensure => directory,
-  }
-  file { '/var/www/index.html':
-    ensure => file,
-    source => 'puppet:///modules/nginx/index.html',
-  }
-  file { '/etc/nginx/nginx.conf':
+  
+  file { "${confdir}/nginx.conf":
     ensure => file,
     source => 'puppet:///modules/nginx/nginx.conf',
-    require => Package['nginx'],
-    notify => Service['nginx'],
   }
+  
   file { '/etc/nginx/conf.d/default.conf':
     ensure => file,
     source => 'puppet:///modules/nginx/default.conf',
-    notify => Service['nginx'],
-    require => Package['nginx'],
   }
+  
   service { 'nginx':
     ensure => running,
     enable => true,
+  }
+  
+  file { '/var/www/':
+    ensure => directory,
+  }
+  
+  file { '/var/www/index.html':
+    ensure => file,
+    source => 'puppet:///modules/nginx/index.html',
   }
 }
